db87085b843a8d6577b65e52bbaf0163322e9b1c
Youssef hesham <capoyoussef16@gmail.com>
2025-11-22 17:37:31 +0200

    add Question

--- src/main/java/Backend/Models/Question.java
+++ src/main/java/Backend/Models/Question.java
@@ -4,10 +4,87 @@
  */
 package Backend.Models;
 
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
- * @author HP_Laptop
+ * @author Pc
  */
 public class Question {
-    
+    private int questionId;
+    private String text;
+    private ArrayList<String> choices;
+    private int correctChoice;
+    public Question(int questionId,String text,ArrayList<String> choices,int correctChoice){
+        setQuestionId(questionId);
+        setCorrectChoice(correctChoice);
+        setText(text);
+        setChoices(choices);
+        
+    }
+    public Question(JSONObject json){
+        this.questionId=json.getInt("questionId");
+        this.text=json.getString("text");
+        this.correctChoice=json.getInt("correctChoices");
+        this.choices=new ArrayList<>();
+        JSONArray choicesArr=json.getJSONArray("choices");
+        if(choicesArr!=null){
+            for(int i=0;i<choicesArr.length();i++){
+                choices.add(choicesArr.getString(i));
+            }
+        }
+    }
+    public JSONObject toJson(){
+        JSONObject obj=new JSONObject();
+        obj.put("questionId", questionId);
+        obj.put("text", text);
+        obj.put("correctChoices", correctChoice);
+        JSONArray choicesArr=new JSONArray();
+        for(int i=0;i<choices.size();i++){
+            choicesArr.put(choices.get(i));
+        }
+        obj.put("choices", choicesArr);
+        return obj;
+    }
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+
+    public int getCorrectChoice() {
+        return correctChoice;
+    }
+    public void setQuestionId(int questionId){
+    if(questionId<0){
+        throw new IllegalArgumentException("questionId must be greater than 0");
+    }
+    this.questionId=questionId;
+    }
+    public void setCorrectChoice(int correctChoice){
+        if(correctChoice<0||correctChoice>=choices.size()){
+           throw new IllegalArgumentException("correctChoice is out of range"); 
+        }
+        this.correctChoice=correctChoice;
+    }
+    public void setText(String text){
+    if(text==null||text.trim().isEmpty()){    
+      throw new IllegalArgumentException("Question text cannot be empty.");   
+    }
+    this.text=text;
+    }
+    public void setChoices(ArrayList<String> choices){
+        if(choices==null||choices.size()>4){
+     throw new IllegalArgumentException("the choices must not exceed than four choices");       
+    }
+        this.choices=choices;
+    }
 }
